2098cf18535e9f40e12a20fa13dfa31e6a4b04e4
b10856039 <ycenz234@gmail.com>
2025-02-03 00:20:49 +0800

    add new cors allow online url

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowLocalhost", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")  // 設定允許的來源
+        policy.WithOrigins("http://localhost:5173",
+                            "https://chat-web-app-vercel.vercel.app")  // 設定允許的來源
               .AllowAnyHeader()                   // 允許所有標頭
               .AllowAnyMethod()                   // 允許所有方法
               .AllowCredentials();       
