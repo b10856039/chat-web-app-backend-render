1315eadba11f51af8cddfb3ef0ed08e04ec4b8ee
b10856039 <ycenz234@gmail.com>
2025-07-08 10:50:47 +0800

    回復前一狀態

--- Program.cs
+++ Program.cs
@@ -15,16 +15,15 @@ var builder = WebApplication.CreateBuilder(args);
 // 添加 CORS 服務
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowFrontend", policy =>
+    options.AddPolicy("AllowLocalhost", policy =>
     {
-        policy.WithOrigins(
-            "http://localhost:5173",
-            "https://chat-web-app-vercel.vercel.app",
-            "https://chat-web-app-vercel-ptik9j4yt-changshengs-projects.vercel.app",
-            "https://chat-web-app-vercel-git-main-changshengs-projects.vercel.app"
-        )
-        .AllowAnyHeader()
-        .AllowAnyMethod();
+        policy.WithOrigins("http://localhost:5173",
+                            "https://chat-web-app-vercel-ptik9j4yt-changshengs-projects.vercel.app",
+                            "https://chat-web-app-vercel-git-main-changshengs-projects.vercel.app",
+                            "https://chat-web-app-vercel.vercel.app")  // 設定允許的來源
+              .AllowAnyHeader()                   // 允許所有標頭
+              .AllowAnyMethod()                   // 允許所有方法
+              .AllowCredentials();       
     });
 });
 
@@ -85,40 +84,21 @@ var app = builder.Build();
 // 設定路由
 app.UseRouting();  // 確保 SignalR 和控制器都能正確處理請求
 
-
-// OPTIONS 請求檢查 (預檢 403/404)
-app.Use(async (context, next) =>
-{
-    if (context.Request.Method == HttpMethods.Options)
-    {
-        context.Response.Headers.Append("Access-Control-Allow-Origin", context.Request.Headers["Origin"]);
-        context.Response.Headers.Append("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        context.Response.Headers.Append("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        context.Response.Headers.Append("Access-Control-Allow-Credentials", "true");
-        context.Response.StatusCode = 200;
-        await context.Response.CompleteAsync();
-    }
-    else
-    {
-        await next();
-    }
-});
-
 // 設置 CORS 策略
 // 在 UseRouting 之前設置 CORS 策略
-app.UseCors("AllowFrontend");  
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-// 使用 MVC 路由
-app.MapControllers();
+app.UseCors("AllowLocalhost");  
 
 // 設置 SignalR Hub 路由
 app.MapHub<ChatHub>("/api/chatHub");
 
 // 使用錯誤處理MiddleWare
 app.UseMiddleware<ExceptionMiddleware>(); // 註冊全域錯誤處理
+
+// 使用 MVC 路由
+app.MapControllers();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 // 如果有需要，可以在此執行資料庫遷移
 await app.MigrateDBAsync();
